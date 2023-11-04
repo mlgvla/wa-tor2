41fa0767e248e0e97c07fe2fce957df43548cac5
Monica Gerard <mlgvla@gmail.com>
2023-11-04 16:44:26 -0400

    stubs out Creature methods

--- creature.py
+++ creature.py
@@ -1,2 +1,23 @@
+# Directional Constants
+UP = 0
+RIGHT = 1
+DOWN = 2
+LEFT = 3
+
 class Creature:
-    pass
+    # Initialize a new creature
+    def __init__(self, type, color):
+        self.x = 0
+        self.y = 0
+        self.type = type
+        self.age = 0
+    
+    # Set position of creature on wator_map   
+    def locate(self, wator_map, position):
+        pass
+    
+    def draw(self):
+        pass
+    
+    def fname(arg):
+        pass
--- wator.py
+++ wator.py
@@ -27,50 +27,64 @@ Framerate
 random.seed()
 pygame.init()
 
+
 def get_args():
     parser = argparse.ArgumentParser(description="Wa-Tor Parameters")
-    
-    parser.add_argument('-w', '--width', help='Wator Width', default=16, type=int)
-    parser.add_argument('-v', '--height', help='Wator Height', default=9, type=int)
-    parser.add_argument('-f', '--fish_color', help='Fish Color', default='blue', type=str)
-    parser.add_argument('-s', '--shark_color', help='Shark Color', default='green', type=str)
-    parser.add_argument('-o', '--ocean_color', help='Ocean Color', default='lightblue', type=str)
-    parser.add_argument('-z', '--zoom_factor', help='Zoom Factor', default=75, type=int)
-    
+
+    parser.add_argument("-w", "--width", help="Wator Width", default=16, type=int)
+    parser.add_argument("-v", "--height", help="Wator Height", default=9, type=int)
+    parser.add_argument(
+        "-f", "--fish_color", help="Fish Color", default="pink", type=str
+    )
+    parser.add_argument(
+        "-s", "--shark_color", help="Shark Color", default="darkgrey", type=str
+    )
+    parser.add_argument(
+        "-o", "--ocean_color", help="Ocean Color", default="cornflowerblue", type=str
+    )
+    parser.add_argument("-z", "--zoom_factor", help="Zoom Factor", default=75, type=int)
+
     args = parser.parse_args()
     return args
-    
+
+
+def init_wator_map(dimensions):
+    """
+    # Using list comprehension
+        rows = 50
+        cols = 100
+        array_2d = [[0 for _ in range(cols)] for _ in range(rows)]
+    """
+
+
 def game_loop(args):
     """
     Error checking goes here
     """
-    
+
     # Setup the window and clock
     window_size = (args.width * args.zoom_factor, args.height * args.zoom_factor)
     screen = pygame.display.set_mode(window_size)
     pygame.display.set_caption("Wa-Tor Simulation")
     clock = pygame.time.Clock()
-    
-    
+
     # Main Loop
     running = True
-    
+
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-                
+
         # fill the screen with ocean color
         screen.fill(args.ocean_color)
-        
-        pygame.display.flip()
-        
+
+        pygame.display.update()
+
         clock.tick(60)
 
     pygame.quit()
-    
-    
-    
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     game_loop(get_args())
